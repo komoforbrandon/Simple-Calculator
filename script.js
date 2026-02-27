b8e8af5010d089296bbffa735e85eedf63094643
KBrand <kbrandn25@gmail.com>
2026-02-27 17:15:06 +0100

    changes made in the structure of the code, espescially the script file

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
 const btns = document.querySelectorAll('.btn')
 const display = document.getElementById('input')
 const answerElt = document.getElementById('answer')
-let num1 = 0
-let num2 = 0
-let operator = 0
 btns.forEach(btn => {
   btn.addEventListener('click', () => {
     const newValue = btn.dataset.value
     if (newValue === 'AC') {
       display.textContent = 0
       answerElt.textContent = 0
-      num1 = num2 = 0
     } else if (newValue === '=') {
-      num2 = Number(answerElt.textContent)
-      console.log('Information', num1, num2, operator)
-      const answer = OpertnFunc(num1, num2, operator)
+      const answer = OpertnFunc(display.textContent)
       answerElt.textContent = answer
-      num1 = Number(answerElt.textContent)
       display.textContent = parseFloat(answerElt.textContent)
     } else if (newValue === 'del') {
-      const delVal = delFunc(answerElt.textContent)
-      answerElt.textContent = delVal
+      const delVal = delFunc(display.textContent)
+      display.textContent = delVal
     } else if (newValue === '+/-') {
-      if (answerElt.textContent[0] === '-') {
-        answerElt.textContent = answerElt.textContent.slice(1)
+      if (display.textContent[0] === '-') {
+        display.textContent = display.textContent.slice(1)
       } else {
-        answerElt.textContent = '-' + answerElt.textContent
+        display.textContent = '-' + display.textContent
       }
-    } else if (/[+\-*%/]/.test(newValue)) {
-      if (num1 === 0) {
-        num1 = Number(answerElt.textContent)
-      }
-      operator = newValue
-      answerElt.textContent = ''
     } else {
-      const displayVal = readValue(newValue, answerElt.textContent)
-      answerElt.textContent = displayVal
+      const displayVal = readValue(newValue, display.textContent)
+      display.textContent = displayVal
     }
   })
 })
@@ -54,22 +41,99 @@ function delFunc (delval) {
 function readValue (newval, dispVal) {
   if (dispVal === '0') {
     return newval
+  } else if (/[+\-*/]/.test(newval) && /[+\-*/]/.test(dispVal.at(-1))) {
+    if (/[+*/]/.test(dispVal.at(-1)) && newval === '-') {
+      dispVal += newval
+      return dispVal
+    } else {
+      if (/[+\-*/]/.test(dispVal.at(-2))) {
+        dispVal = dispVal.slice(0, -2) + newval
+      } else {
+        dispVal = dispVal.slice(0, -1) + newval
+      }
+      return dispVal
+    }
   } else {
     dispVal += newval
     return dispVal
   }
 }
 
-function OpertnFunc (num1, num2, operator) {
-  if (operator === '+') {
-    return num1 + num2
-  } else if (operator === '-') {
-    return num1 - num2
-  } else if (operator === '*') {
-    return num1 * num2
-  } else if (operator === '/') {
-    return num1 / num2
-  } else if (operator === '%') {
-    return num1 % num2
+function OpertnFunc (expression) {
+  const result = evaluateExpression (expression)
+  function evaluateExpression(expr) {
+    console.log('This is the expression', expr)
+    const normalize = normaliseExpr(expr)
+    const arrOfChar = strToArr(normalize)
+    const calMultDiv = perfMultDiv(arrOfChar)
+    console.log('This is the return value before addSub:', calMultDiv)
+    const calAddSub = perfAddSub(calMultDiv)
+    console.log('Return Array is : ', calAddSub)
+    return calAddSub[0];
   }
+
+  function normaliseExpr (expr) {
+    let x = expr.replace(/\+-/g, '-')
+    return x
+  }
+
+  function strToArr (expr) {
+    let x = expr.match(/\d+(\.\d+)?|[+\-*/()]/g)
+    let normArr = []
+    for (let i = 0; i < x.length; i++) {
+      if (i === 0 && x[i] === '-') {
+        let negNum = '-' + x[i + 1]
+        normArr.push(Number(negNum))
+        i++
+      } else if ((x[i] === '*' || x[i] === '/') && x[i + 1] === '-' && !isNaN(x[i + 2])) {
+        normArr.push(x[i])
+        let negNum = '-' + x[i + 2]
+        normArr.push(Number(negNum))
+        i += 2
+      }
+      else if (!isNaN(x[i])) {
+        normArr.push(Number(x[i]))
+      }
+      else {
+        normArr.push(x[i])
+      }
+    }
+    return normArr;
+  }
+
+  function perfMultDiv (x) {
+    let i = 0
+    while (i < x.length) {
+      if (x[i] === '*') {
+        let multValue = x[i - 1] * x[i + 1]
+        x.splice(i - 1, 3, multValue)
+      } else if (x[i] === '/') {
+        let multValue = x[i - 1] / x[i + 1]
+        x.splice(i - 1, 3, multValue)
+      } else if (x[i] === '%') {
+        let modValue = x[i - 1] % x[i + 1]
+        x.splice(i - 1, 3, modValue)
+      } else {
+        i++
+      }
+    }
+    return x
+  }
+
+  function perfAddSub (x) {
+    let i = 0
+    while (i < x.length) {
+      if (x[i] === '+') {
+        let addValue = x[i - 1] + x[i + 1]
+        x.splice(i - 1, 3, addValue)
+      } else if (x[i] === '-') {
+        let subValue = x[i - 1] - x[i + 1]
+        x.splice(i - 1, 3, subValue)
+      } else {
+        i++
+      }
+    }
+    return x;
+  }
+  return result
 }
